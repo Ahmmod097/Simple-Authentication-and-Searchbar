cb55a4060c0e13c787e57bad11b35daaa62ed90f
Ahmmod097 <55923593+Ahmmod097@users.noreply.github.com>
2021-09-29 22:06:31 +0630

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,8 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-k@&g%1h%$5z1^8*i06w9&r&t_a5#t72$k3-u(kf-pifbr2el3^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-
+DEBUG = True
 ALLOWED_HOSTS = ['127.0.0.1','ssimpleauth.herokuapp.com']
 
 
